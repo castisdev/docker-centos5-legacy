590c9550e53bfe1141aab6701611cfc619044773
sasgas <sasgas@castis.com>
2016-10-26 10:24:50 +0900

    cppcheck 를 yum 에서 설치

--- install_cppcheck1761.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-cd ~
-wget --no-check-certificate --content-disposition https://sourceforge.net/projects/cppcheck/files/cppcheck/1.76.1/cppcheck-1.76.1.tar.bz2/download
-tar xf cppcheck-1.76.1.tar.bz2
-cd cppcheck-1.76.1
-mkdir build;cd build;cmake ..;make install
-cd ~
-rm -rf cppcheck*
