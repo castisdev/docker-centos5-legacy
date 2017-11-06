02d48afae5feae34f7dd3fd46b2f98b82899fede
sasgas <sasgas@castis.com>
2017-11-06 14:58:42 +0900

    get-pip.py 추가 centos5 에서 https 로 get-pip.py 파일 다운로드가 안됨

--- install_cpptools.sh
+++ install_cpptools.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -e
 
-wget https://bootstrap.pypa.io/get-pip.py --no-check-certificate
-python2.7 get-pip.py
 pip2.7 install gcovr
-rm -rf get-pip.py
 
 wget https://github.com/google/styleguide/raw/gh-pages/cpplint/cpplint.py --no-check-certificate
 sed -i -e s/'\/usr\/bin\/env python'/'\/usr\/bin\/env python2.7'/g cpplint.py
